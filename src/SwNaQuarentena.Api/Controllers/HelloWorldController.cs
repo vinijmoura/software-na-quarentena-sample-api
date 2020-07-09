79b3625fd5257a3d7acf4f334f04f36fbdf3d762
William <williamsrz@outlook.com>
2020-07-08 22:56:12 -0300

    🎉initial commit

---
+++ src/SwNaQuarentena.Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SwNaQuarentena.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get() => 
+            "Works on my machine!";
+        
+    }
+}
